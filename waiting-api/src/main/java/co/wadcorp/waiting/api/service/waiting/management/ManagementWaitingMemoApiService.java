d63440b9730c2b77711debc70e4ecd46d9cc9f50
yjlee <yjlee@catchtable.co.kr>
2023-04-26 18:53:06 +0900

    [test2] rebase2

---
+++ waiting-api/src/main/java/co/wadcorp/waiting/api/service/waiting/management/ManagementWaitingMemoApiService.java
@@ -0,0 +1,15 @@
+package co.wadcorp.waiting.api.service.waiting.management;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ManagementWaitingMemoApiService {
+
+  public Object save(String shopId, Object serviceRequest) {
+    return null;
+  }
+
+  public void delete(String waitingId) {
+
+  }
+}
