9bfabd6daedb3e1a4b29a5e11d2a2f4c2191c122
yjlee <yjlee@catchtable.co.kr>
2023-04-27 11:04:39 +0900

    [test] occurs conflict

--- waiting-api/src/main/java/co/wadcorp/waiting/api/service/waiting/management/ManagementWaitingMemoApiService.java
+++ waiting-api/src/main/java/co/wadcorp/waiting/api/service/waiting/management/ManagementWaitingMemoApiService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 public class ManagementWaitingMemoApiService {
 
   public Object save(String shopId, Object serviceRequest) {
+    System.out.println("conflict!!!");
     return null;
   }
 
